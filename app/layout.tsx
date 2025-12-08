c0960eba183882da9b63c2e3c49a64134910de1a
mohitkumar7895 <mohitporwal596@gmail.com>
2025-12-08 20:21:14 +0530

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata, Viewport } from 'next'
 import './globals.css'
+import Head from 'next/head'
+
+
 
 export const metadata: Metadata = {
   title: 'Jan Seva Kendra - Arpit Porwal',
   description: 'All Government & Private Services Under One Roof',
+  other: {
+    'google-site-verification': '2rT4H0NwQhMpGMYTHC_rnHNs7Cpatm4pSUhvhtfaRPw',
+  },
 }
+
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -19,7 +26,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="2rT4H0NwQhMpGMYTHC_rnHNs7Cpatm4pSUhvhtfaRPw" />
+
+      </Head>
       <body className="min-h-screen bg-white">{children}</body>
+      
     </html>
   )
 }
